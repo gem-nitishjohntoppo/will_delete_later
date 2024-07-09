0718eaaebf7d9e9f711346a0b8f15d9c3f35edd7
gem-nitishjohntoppo <nitish.toppo@geminisolutions.com>
2024-07-09 13:54:56 +0530

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
+# Define the RTC configuration with the STUN server
+RTC_CONFIGURATION = RTCConfiguration({
+    "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+})
 
 def main():
     st.title("Webcam Feed using WebRTC")
 
-    webrtc_streamer(key="example")
-
+    webrtc_streamer(
+        key="example",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION
+    )
 
 if __name__ == "__main__":
     main()
